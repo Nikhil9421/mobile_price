83429b792c251980f0b7d5af77c708510bfcb86b
Nikhil9421 <nikhilpajgade22@gmail.com>
2024-03-06 21:07:04 +0530

    added updated files

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ def predicted_price_class():
         wifi = eval(data['wifi'])
 
         result = get_predicted_price_class(battery_power, blue, clock_speed, dual_sim, fc,four_g, int_memory, m_dep,mobile_wt,n_cores,pc,px_height,px_width,ram,sc_h,sc_w,talk_time,three_g,touch_screen,wifi)
-        price = {0: '10-16k', 1: '18-24k', 2: '25-30k', 3: '30-35k'}
-        print("price",result)
-    return render_template('index.html', prediction=f"{price[result]}")
+        # price = {0: '10-16k', 1: '18-24k', 2: '25-30k', 3: '30-35k'}
+        # print("price",result)
+    return render_template('index.html', prediction=result)
     #return render_template('index.html')    
     #prediction=f"{price[result]}"
 
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_predicted_price_class(battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep,mobile_wt,n_cores,pc,px_height,px_width,ram,sc_h,sc_w,talk_time,three_g,touch_screen,wifi):
-    mobile_path = r"C:\Users\HP\Desktop\Python and Data Science\Project on Mobile Data Set\data\knn_model.pkl"
+    mobile_path = r"data/knn_model.pkl"
 
 
     with open(mobile_path,'rb') as f:
@@ -14,7 +14,17 @@ def get_predicted_price_class(battery_power, blue, clock_speed, dual_sim, fc, fo
         # print(test_array)
         predicted_price_class = model.predict(test_array)[0]
         print("predicted_price_class :",predicted_price_class)
+
+        if predicted_price_class == 3:
+            price = "30-35k"
+        elif predicted_price_class == 2:
+            price = "25-30k"
+        elif predicted_price_class == 1:
+            price = "18-24k"
+        else:
+            price = "10-16k"
+        print("Mobile price: ", price)
         
         
         
-        return predicted_price_class
+        return price
